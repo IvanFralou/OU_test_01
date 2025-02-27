166315487d89d2b3b9209845f381b64b3a0056a0
258477 <258477@edu.p.lodz.pl>
2025-02-27 18:42:13 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    auto lang = "C++";
+    cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
+
